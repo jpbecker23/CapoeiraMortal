ab864281bc60929612089948fc51f7a377d2bad9
João Pedro Becker <jpbecker007@gmail.com>
2025-11-06 00:22:22 -0300

    refactor: correção das animações
    inicio de novo repositório

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator PlayerAnim;
+    public bool isAttacking = false;
+
+
+    void Start()
+    {
+        PlayerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleActions();
+    }
+    private void HandleActions()
+    {
+        // Chute Alto (K)
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            PlayerAnim.SetBool("IsAttacking", true);
+            PerformBencao();
+        }
+
+        /*         // Ponteira - Chute Baixo (S)
+                if (Input.GetKeyDown(KeyCode.L) && !isDodging && !isAttacking && gameOver == false)
+                {
+                    PerformLowKick();
+                } */
+    }
+    private void PerformBencao()
+    {
+        // mark state and trigger animation logic
+        // isAttacking = true;
+        PlayerAnim.SetTrigger("Bencao");
+    }
+
+}
