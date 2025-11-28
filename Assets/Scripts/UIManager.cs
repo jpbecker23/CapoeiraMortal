f9cbf753e45230a102ced4536939fbab091aa78d
João Pedro Becker <jpbecker007@gmail.com>
2025-11-28 15:03:42 -0300

    refactor: add level3 and ui sprites
    adcionei mais um nivel e tentei mexer na ui, com sprites youwin e nextlevel, mas não vou mais mexer nisso pq tenho que estudar!!! vao tomar no cu, obrigado.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class GerenciadorDeCena : MonoBehaviour
 {
     public GameObject PauseUI;
+    public GameObject NextLevelPanel;
 
     public void Update()
     {
@@ -31,9 +32,22 @@ public class GerenciadorDeCena : MonoBehaviour
         Time.timeScale = 1f;
     }
 
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
     public void ExitGame()
     {
         Application.Quit();
         UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void NextLevel()
+    {
+        NextLevelPanel.SetActive(true);
+        //fazer logica de delay ou algo assim para primeiro carregar o sprite "YOUWIN" e depois o "NEXTLEVEL"
+        Time.timeScale = 0f;
     }
 }
